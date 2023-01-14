65538e6ff4c709fdb3f9553630f21c694b64aa7c
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-01-14 15:58:01 +0000

    page link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import Pagination from './components/Pagination';
 import './App.css';
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const lastPage = 3;
+
   return (
     <div className="container">
       <h1>React TypeScript Pagination</h1>
+      <Pagination
+        currentPage={currentPage}
+        lastPage={lastPage}
+        maxLength={7}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
